b4a6aae9ba7160321944a3bebfde0afa035dcd39
hanantawafsha <hananjtawafsha@gmail.com>
2025-01-19 20:52:35 -0600

    upload navbar

--- src/components/user/navbar/Navbar.jsx
+++ src/components/user/navbar/Navbar.jsx
@@ -1,7 +1,27 @@
 import React from 'react'
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
 
-export default function Navbar() {
+export default function CustomNavbar() {
   return (
-    <div>Navbar</div>
+    <>
+    <Navbar expand="lg" className="bg-body-tertiary">
+      <Container>
+        <Navbar.Brand href="#home">eCommerce Website React</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ms-auto">
+            <Nav.Link href="#home">Login</Nav.Link>
+            <Nav.Link href="#link">Rigester</Nav.Link>
+            
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+    </>
   )
 }
+
+
