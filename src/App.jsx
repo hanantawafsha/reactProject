63ab4ac6e40f2678e39d43d5da2b97a733dd0b19
hanantawafsha <hananjtawafsha@gmail.com>
2025-01-19 22:03:36 -0600

    upload folders

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,28 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import CustomNavbar from './components/user/navbar/Navbar'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import AuthLayout from './layout/AuthLayout'
+import Register from './pages/user/register/Register'
+import Login from './pages/user/login/Login'
+import DashboardLayout from './layout/DashboardLayout'
 
 function App() {
-  const [count, setCount] = useState(0)
+const router = createBrowserRouter([
+  { path: '/', 
+    element: <AuthLayout/>,
+    children: [
+      { path: 'register', element: <Register/> },
+      { path: 'login', element: <Login/> },
+    ],
 
+  },
+  { path: '/dashbord', element: <DashboardLayout/>},
+  { path: '*', element: <h1>Page not found</h1> }, // Default route if none of the above match
+])
   return (
     <>
-     <CustomNavbar />
+     <RouterProvider router={router} />
     </>
   )
 }
--- src/components/user/navbar/Navbar.jsx
+++ src/components/user/navbar/Navbar.jsx
@@ -3,18 +3,19 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
+import { Link } from'react-router-dom';
 
 export default function CustomNavbar() {
   return (
     <>
     <Navbar expand="lg" className="bg-body-tertiary">
       <Container>
-        <Navbar.Brand href="#home">eCommerce Website React</Navbar.Brand>
+        <Navbar.Brand href="/">eCommerce Website React</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ms-auto">
-            <Nav.Link href="#home">Login</Nav.Link>
-            <Nav.Link href="#link">Rigester</Nav.Link>
+            <Nav.Link as={Link} to={'/login'}>Login</Nav.Link>
+            <Nav.Link as={Link} to={'/register'}>Rigester</Nav.Link>
             
           </Nav>
         </Navbar.Collapse>
